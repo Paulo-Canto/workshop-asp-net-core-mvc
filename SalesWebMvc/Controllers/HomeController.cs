538ac6fe50b8625ff4abb73a83e7caa5bbdb4838
Paulo Canto <pauloteixeira.arthur@gmail.com>
2023-08-17 15:10:24 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC App from C# Course.";
+            ViewData["Professor"] = "professor Nelio Alves (C# Course)";
 
             return View();
         }
