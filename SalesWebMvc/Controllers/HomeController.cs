793b9172693c1e3b5cced034587f04ca374f02ce
Paulo Canto <pauloteixeira.arthur@gmail.com>
2023-08-22 16:02:46 -0300

    Asynchronous operations using Tasks (async, await)

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using SalesWebMvc.Models;
 using SalesWebMvc.Models.ViewModels;
 
 namespace SalesWebMvc.Controllers
@@ -18,8 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Sales Web MVC App from C# Course.";
-            ViewData["Professor"] = "professor Nelio Alves (C# Course)";
+            ViewData["Message"] = "Salles Web MVC App from C# Course";
+            ViewData["Professor"] = "Nelio Alves";
 
             return View();
         }
