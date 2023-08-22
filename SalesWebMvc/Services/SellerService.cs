494541e0e44e9e3b411a1b04e47e58abf04a4357
Paulo Canto <pauloteixeira.arthur@gmail.com>
2023-08-22 13:44:41 -0300

    Simple create form

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -24,7 +24,11 @@ namespace SalesWebMvc.Services
         }
 
 
-
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
 
     }
 }
